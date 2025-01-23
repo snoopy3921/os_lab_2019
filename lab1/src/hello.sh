3863b843dc7d9514353f9b04b418f8890f6377c2
snoopy3921 <giahuy392001@gmail.com>
2025-01-23 13:43:50 +0300

    Lab 1

--- lab1/src/hello.sh
+++ lab1/src/hello.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-
 echo "Hello, world!"
+
+echo "Current working dir is $PWD"
+
+echo -n "Current time is " 
+
+date
+
+echo "Current path is $PATH"
---
+++ lab1/src/mybash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+	echo "$i sec"
+	sleep 1
+done
+echo "Done!"
