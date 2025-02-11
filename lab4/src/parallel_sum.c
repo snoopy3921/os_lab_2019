66edd80b6fc515383125fe0be0dca8f96dff01dc
snoopy3921 <giahuy392001@gmail.com>
2025-02-11 18:15:00 +0300

    Lab 4 done

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <pthread.h>
+#include <getopt.h>
 
 struct SumArgs {
   int *array;
@@ -10,15 +11,25 @@ struct SumArgs {
   int end;
 };
 
+void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
+  srand(seed);
+  for (int i = 0; i < array_size; i++) {
+    array[i] = rand();
+  }
+}
+
 int Sum(const struct SumArgs *args) {
   int sum = 0;
   // TODO: your code here 
+  for(int i = args->begin; i < args->end - 1; i++) {
+    sum += args->array[i];
+    }
   return sum;
 }
 
 void *ThreadSum(void *args) {
   struct SumArgs *sum_args = (struct SumArgs *)args;
-  return (void *)(size_t)Sum(sum_args);
+  return (void *)(size_t)Sum(sum_args); /* We can instead calling pthread_exit(Sum(sum_args))*/
 }
 
 int main(int argc, char **argv) {
@@ -32,8 +43,40 @@ int main(int argc, char **argv) {
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
-  pthread_t threads[threads_num];
 
+  static struct option options[] = {{"seed", required_argument, 0, 's'},
+    {"array_size", required_argument, 0, 'a'},
+    {"threads_num", required_argument, 0, 't'},
+    {0, 0, 0, 0}};
+  
+  int option_idx = 0;
+  int c;
+  while((c = getopt_long(argc, argv, "s:a:t:", options, &option_idx)) != -1)
+  {
+    switch (c)
+    {
+    case 's':
+      /* code */
+      printf("Seed: %s\n", optarg);
+      seed = atoi(optarg);
+      break;
+    case 'a':
+      /* code */
+      printf("Array Size: %s\n", optarg);
+      array_size = atoi(optarg);
+      break;
+    case 't':
+      /* code */
+      printf("Thread num: %s\n", optarg);
+      threads_num = atoi(optarg);
+      break;
+    
+    default: /* Case '?' */
+      break;
+    }
+  }
+
+  pthread_t threads[threads_num];
   /*
    * TODO:
    * your code here
@@ -41,9 +84,13 @@ int main(int argc, char **argv) {
    */
 
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed);
 
   struct SumArgs args[threads_num];
   for (uint32_t i = 0; i < threads_num; i++) {
+    args->array = array;
+    args->begin = i * array_size / threads_num;
+    args->end = (i + 1) * array_size / threads_num;
     if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
       printf("Error: pthread_create failed!\n");
       return 1;
